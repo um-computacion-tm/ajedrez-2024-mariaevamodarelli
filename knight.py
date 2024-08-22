fe4d8e84d31bd1e5ae42c12c24194f23edaa4465
Eva <modarellimariaeva@gmail.com>
2024-08-21 22:57:19 -0300

    agregando posiciones de piezas

--- knight.py
+++ knight.py
@@ -2,5 +2,8 @@ from pieces import Piece
 
 
 class Knight(Piece):
-    ...
-    
+    def __str__(self):
+        if self.__color__ == "WHITE":
+            return "♞"
+        else:
+            return "♘"
